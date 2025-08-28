6249794696b3843e6c8d4fd4362a9d8b37ed30a0
unknown <vishalshakaya@gmail.com>
2025-08-28 16:50:40 +0530

    remove run server files

--- soft_run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Interior Bazzar Django Server...
-REM Start the server
-echo Starting server on http://127.0.0.1:8888/
-python -m uvicorn interior_bazzar.asgi:application --port 8888 --host 0.0.0.0
-
-pause 
